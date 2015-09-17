617ea249221e91929593daa33262d2ae721b2cdb
dipesh09 <200298834@student.georgianc.on.ca>
2015-09-17 16:40:56 +0000

    Added helloworld.sh initial file

---
+++ helloworld.sh
@@ -0,0 +1,9 @@
+#!/bin/bash                                                                                                                                                                                           
+
+# This script prints out Hello World                                                                                                                                                                  
+
+# It was written by Dipesh Patel                                                                                                                                                                      
+
+                                                                                                                                                                                                      
+
+echo Hello world     
